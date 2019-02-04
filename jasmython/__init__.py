f1682c8546bc9b8cb2890870c6a37b2eb8a2d86c
R0bertus <hans.gillis.it@gmail.com>
2019-02-04 20:00:09 +0100

    Yummy!

--- jasmython/__init__.py
+++ jasmython/__init__.py
@@ -1,10 +1,21 @@
 from browser import window
 describe = window.describe
+test = window.jasmine.getEnv().it
+before_each = window.jasmine.getEnv().beforeEach
+
 
 class Suite(object):
-    def __init__(self, name, fnc):
-        if name is not None and fnc is not None:
-            self.name = name
-            self.suite = describe(self.name, fnc)
+    def __init__(self, name):
+        self.name = name
 
+    def group(self, fnc):
+        describe(self.name, fnc)
 
+    def test(self, name, callback):
+        test(name, callback)
+
+    def suite(self, name, fnc):
+        describe(name, fnc)
+
+    def before_each(self, callback):
+        before_each(callback)
--- spec/PizzaSpec.py
+++ spec/PizzaSpec.py
@@ -1,13 +1,22 @@
 import jasmython as jasmine
 from browser import window
 expect = window.expect
-test = window.jasmine.getEnv().it
 
+def setup(objects):
+    [o.group(o.suites[i]) for i, o in enumerate(objects)]
 
 class Pizza(jasmine.Suite):
-    def __init__(self, name=None, fnc=None, slices=1):
-        super().__init__(name, fnc)
+    def __init__(self, name="General suite", slices=1):
+        super().__init__(name)
+        self.initial_slices = slices
         self.slices = slices
+        self.suites = [
+            self.suite_1,
+            self.suite_2
+        ]
+
+    def reset(self):
+        self.slices = self.initial_slices
 
     def get_slices(self):
         return self.slices
@@ -24,16 +33,26 @@ class Pizza(jasmine.Suite):
 
     def i_cannot_eat_anymore_slices(self):
         while self.eat_a_slice():
-            pass
+            print("Yummy in my Tummy!", self.get_slices())
 
         expect(self.get_slices()).toBe(0)
 
+    def suite_1(self):
+        self.test("My pizza should have at least 1 slice", self.i_can_eat_a_slice)
+        self.test("My pizza has no slices left", self.i_cannot_eat_anymore_slices)
+        self.test("My pizza does not reset, i'm hungry bro", self.i_cannot_eat_anymore_slices)
 
-def main():
-    pizza = Pizza()
-    test("My pizza should have at least 1 slice", pizza.i_can_eat_a_slice)
-    test("My Pizza has no slices left", pizza.i_cannot_eat_anymore_slices)
+    def suite_2(self):
+        self.suite("Nested Suite", Pizza(slices=25).suite_1)
+        self.before_each(self.reset)
+        self.test("My pizza has no slices left", self.i_cannot_eat_anymore_slices)
+        self.test("My pizza should have at least 1 slice", self.i_can_eat_a_slice)
+        self.test("My pizza resets, going to eat it all!", self.i_cannot_eat_anymore_slices)
 
 
 if __name__ == '__main__':
-    Pizza("Pizza Suite", main, 1)
+    setup([
+        Pizza("Pizza Salami", slices=3),
+        Pizza("Pizza BBQ", slices=6)
+    ])
+
